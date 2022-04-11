0c1f0a3d2228dbee080e8e3af9dac595d102dec6
Arsham <arsham.ramezani@gmail.com>
2022-04-11 18:34:06 +0430

    feat: head table added

--- src/pages/upcoming/UpcomingHeadTable.js
+++ src/pages/upcoming/UpcomingHeadTable.js
@@ -9,7 +9,30 @@ const UpcomingHeadTable = () => {
         <div className={classes.collections}>
           <Typography className={classes.text}>collections</Typography>
         </div>
+        <div className={classes.twitterFollowers}>
+          <Typography className={classes.text}>Twitter followers</Typography>
+        </div>
+        <div className={classes.discordFollowers}>
+          <Typography className={classes.text}>Discord Followers</Typography>
+        </div>
+        <div className={classes.communityScore}>
+          <Typography className={classes.text}>Community Score</Typography>
+        </div>
+        <div className={classes.mintPrice}>
+          <Typography className={classes.text}>Mint Price</Typography>
+        </div>
+        <div className={classes.publicPrice}>
+          <Typography className={classes.text}>Public Price</Typography>
+        </div>
+        <div className={classes.added}>
+          <Typography className={classes.text}>Added</Typography>
+        </div>
+        <div className={classes.upvotes}>
+          <Typography className={classes.text}>Upvotes</Typography>
+        </div>
+        <div className={classes.end} />
       </div>
+      <div style={{ width: 1440 }} />
     </div>
   )
 }
--- src/pages/upcoming/styles/UpcomingHeadTable.style.js
+++ src/pages/upcoming/styles/UpcomingHeadTable.style.js
@@ -11,12 +11,15 @@ const useStyles = makeStyles(() => ({
     minWidth: 'auto',
     height: 'auto',
     backgroundColor: 'rgb(11, 30, 57)',
-    borderTopLeftRadius: '0.75rem',
-    borderBottomLeftRadius: '0.75rem'
+    borderRadius: '0.75rem'
   },
   text: {
     fontSize: 16,
     color: 'rgba(255,255,255,0.8)'
+    // textJustify: 'center'
+    // verticalAlign: 'middle',
+    // textAlign: 'end'
+    // display: 'flex'
   },
   collections: {
     boxSizing: 'border-box',
@@ -28,11 +31,96 @@ const useStyles = makeStyles(() => ({
     padding: '1.75rem 1.25rem',
     borderTopLeftRadius: '0.75rem',
     borderBottomLeftRadius: '0.75rem'
+  },
+  twitterFollowers: {
+    boxSizing: 'border-box',
+    flex: '84 0 auto',
+    minWidth: 0,
+    width: 84,
+    cursor: 'pointer',
+    paddingRight: '1.25rem',
+    paddingLeft: '0.5rem',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  discordFollowers: {
+    boxSizing: 'border-box',
+    flex: '84 0 auto',
+    minWidth: 0,
+    width: 84,
+    cursor: 'pointer',
+    paddingRight: '1.25rem',
+    paddingLeft: '0.5rem',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  communityScore: {
+    boxSizing: 'border-box',
+    flex: '100 0 auto',
+    minWidth: 0,
+    width: 100,
+    cursor: 'pointer',
+    paddingRight: '1.25rem',
+    paddingLeft: '0.5rem',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  mintPrice: {
+    boxSizing: 'border-box',
+    flex: '120 0 auto',
+    minWidth: 0,
+    width: 120,
+    cursor: 'pointer',
+    paddingRight: '1.25rem',
+    paddingLeft: '0.5rem',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  publicPrice: {
+    boxSizing: 'border-box',
+    flex: '170 0 auto',
+    minWidth: 0,
+    width: 170,
+    cursor: 'pointer',
+    // paddingRight: '1.25rem',
+    paddingLeft: 20,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  added: {
+    boxSizing: 'border-box',
+    flex: '170 0 auto',
+    minWidth: 0,
+    width: 160,
+    cursor: 'pointer',
+    // paddingRight: '1.25rem',
+    paddingLeft: 20,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  upvotes: {
+    boxSizing: 'border-box',
+    flex: '80 0 auto',
+    minWidth: 80,
+    width: 80,
+    cursor: 'pointer',
+    // paddingRight: '1.25rem',
+    paddingLeft: 20,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  end: {
+    boxSizing: 'border-box',
+    flex: '72 0 auto',
+    minWidth: 72,
+    width: 72,
+    cursor: 'pointer',
+    borderTopRightRadius: '0.75rem',
+    borderBottomRightRadius: '0.75rem'
   }
 }))
 export default useStyles
-const fibo = (number) => {
-  if (number > 2) {
-    return fibo(number - 2) + fibo(number - 1)
-  } else return 1
-}
