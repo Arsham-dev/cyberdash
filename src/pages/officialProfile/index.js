203890544d728c0b66785941695092e1d07feadf
Arsham <arsham.ramezani@gmail.com>
2022-05-03 18:39:07 +0430

    feat: tabs added

--- src/pages/officialProfile/index.js
+++ src/pages/officialProfile/index.js
@@ -1,15 +1,50 @@
+import { Paper, Tab, Tabs } from '@material-ui/core'
+import { useState } from 'react'
 import OfficialProfileInfo from './OfficialProfileInfo'
 import OfficialProfileTopPart from './OfficialProfileTopPart'
+import ChartIcon from '@material-ui/icons/BarChart'
 import OfficialProfileTopPartSocialMediaContainer from './OfficialProfileTopPartSocialMediaContainer'
+import DashboardIcon from '@material-ui/icons/DashboardOutlined'
 import useStyles from './styles/index.style'
 
 const OfficialProfile = () => {
   const classes = useStyles()
+  const [tabsValue, settabsValue] = useState()
   return (
     <div className={classes.root}>
       <OfficialProfileTopPart />
       <OfficialProfileTopPartSocialMediaContainer />
       <OfficialProfileInfo />
+      <Paper className={classes.paper}>
+        <Tabs
+          value={tabsValue}
+          onChange={(event, newValue) => settabsValue(newValue)}
+          indicatorColor="secondary"
+          className={classes.tabs}
+          classes={{ indicator: classes.indicator }}
+          // classes={{}}
+          // TabIndicatorProps={{className:}}
+          textColor="primary"
+          centered>
+          <Tab
+            label={
+              <div className={classes.tabLabel}>
+                <DashboardIcon />
+                Trading Dashboard
+              </div>
+            }
+          />
+          <Tab
+            label={
+              <div className={classes.tabLabel}>
+                <ChartIcon />
+                Analytical Charts
+              </div>
+            }
+          />
+          {/* <Tab label="Item Three" /> */}
+        </Tabs>
+      </Paper>
     </div>
   )
 }
--- src/pages/officialProfile/styles/index.style.js
+++ src/pages/officialProfile/styles/index.style.js
@@ -6,6 +6,29 @@ const useStyles = makeStyles(() => ({
     width: '100%',
     flexDirection: 'column'
   },
- 
+  paper: {
+    marginTop: 70,
+    flexGrow: 1,
+    backgroundColor: 'rgb(0, 15, 36)'
+  },
+  indicator: {
+    backgroundColor: 'rgb(25, 86, 226)',
+    height: 5
+  },
+  tabs: {
+    borderBottom: '1.5px solid rgb(25, 86, 226)'
+  },
+  tabLabel: {
+    display: 'flex',
+    alignItems: 'center',
+    '& svg': {
+      color: '#FFF',
+      fontSize: 24
+    },
+    font: 'normal normal bold 17px/20px Roboto',
+    columnGap: 4,
+    color: '#FFF',
+    textTransform: 'capitalize'
+  }
 }))
 export default useStyles
